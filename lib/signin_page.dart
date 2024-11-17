a426aa5fc1681630f65499c1464f3a32e1a7b5a6
Aloyce-Kimata <kimataaloyce444@gmail.com>
2024-11-17 11:08:01 +0300

    pages updates

---
+++ lib/signin_page.dart
@@ -0,0 +1,154 @@
+import 'package:flutter/material.dart';
+import 'package:rides_sharing_app/signup_page.dart';
+
+class SigninPage extends StatelessWidget {
+  SigninPage({Key? key}) : super(key: key);
+
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(0),
+        child: AppBar(
+          elevation: 0,
+          backgroundColor: Colors.white,
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Icon(
+                Icons.directions_car,
+                size: 120,
+              ),
+              const SizedBox(height: 50),
+              const Text(
+                'ARDHI UNIVERSITY STUDENT\'S RIDES SHARING APP',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 15.5,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 25),
+              MyTextField(
+                controller: usernameController,
+                hintText: 'Username',
+                obscureText: false,
+              ),
+              const SizedBox(height: 10),
+              MyTextField(
+                controller: passwordController,
+                hintText: 'Password',
+                obscureText: true,
+              ),
+              const SizedBox(height: 25),
+              Container(
+                padding: const EdgeInsets.all(25),
+                margin: const EdgeInsets.symmetric(horizontal: 25),
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: GestureDetector(
+                  onTap: () {
+                    print("Sign in clicked. No navigation performed.");
+                  },
+                  child: const Center(
+                    child: Text(
+                      "Sign in",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 25),
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const SignupPage()),
+                  );
+                },
+                child: const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Not a member?',
+                      style: TextStyle(color: Colors.black),
+                    ),
+                    SizedBox(width: 4),
+                    Text(
+                      'Register now',
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 230),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    '@${DateTime.now().year}, All Rights Reserved',
+                    style: const TextStyle(color: Colors.black),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+// Custom TextField widget for username and password input fields.
+class MyTextField extends StatelessWidget {
+  final TextEditingController controller;
+  final String hintText;
+  final bool obscureText;
+
+  const MyTextField({
+    super.key,
+    required this.controller,
+    required this.hintText,
+    required this.obscureText,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+      child: TextField(
+        controller: controller,
+        obscureText: obscureText,
+        decoration: InputDecoration(
+          enabledBorder: const OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.white),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.grey.shade400),
+          ),
+          fillColor: Colors.grey.shade200,
+          filled: true,
+          hintText: hintText,
+          hintStyle: TextStyle(color: Colors.grey[500]),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/signup_page.dart
@@ -0,0 +1,276 @@
+import 'package:flutter/material.dart';
+
+class SignupPage extends StatefulWidget {
+  const SignupPage({Key? key}) : super(key: key);
+
+  @override
+  _SignupPageState createState() => _SignupPageState();
+}
+
+class _SignupPageState extends State<SignupPage> {
+  String? selectedCategory = "Student";
+  String? selectedVehicleType;
+  String? selectedVehicleColor;
+  String? selectedWorkingLocation;
+
+  final TextEditingController fullNameController = TextEditingController();
+  final TextEditingController phoneNumberController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController licensePlateNumberController =
+      TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController confirmPasswordController =
+      TextEditingController();
+
+  // Method to reset all input field controllers
+  void resetInputFields() {
+    fullNameController.clear();
+    phoneNumberController.clear();
+    emailController.clear();
+    licensePlateNumberController.clear();
+    passwordController.clear();
+    confirmPasswordController.clear();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: true,
+      backgroundColor: Colors.white,
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(0),
+        child: AppBar(
+          elevation: 0,
+          backgroundColor: Colors.white,
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 40),
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              const SizedBox(height: 30),
+              const Text(
+                "Register Now",
+                style: TextStyle(
+                  fontSize: 35,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 30),
+              inputFile(
+                label: "Select Category",
+                options: ["Student", "Driver"],
+                selectedValue: selectedCategory,
+                onChanged: (value) {
+                  setState(() {
+                    selectedCategory = value;
+                    if (value == "Student") {
+                      selectedVehicleType = null;
+                      selectedVehicleColor = null;
+                      selectedWorkingLocation = null;
+                    }
+                  });
+                },
+              ),
+              if (selectedCategory == "Student" ||
+                  selectedCategory == null) ...[
+                inputFile(label: "Full Name", controller: fullNameController),
+                inputFile(
+                    label: "Phone Number", controller: phoneNumberController),
+                inputFile(label: "Email", controller: emailController),
+              ],
+              if (selectedCategory == "Driver") ...[
+                inputFile(label: "Full Name", controller: fullNameController),
+                inputFile(
+                    label: "Phone Number", controller: phoneNumberController),
+                inputFile(label: "Email", controller: emailController),
+                inputFile(
+                  label: "Vehicle Type",
+                  options: ["Taxi", "Bajaji", "XL"],
+                  selectedValue: selectedVehicleType,
+                  onChanged: (value) {
+                    setState(() {
+                      selectedVehicleType = value;
+                    });
+                  },
+                ),
+                inputFile(
+                  label: "Vehicle Color",
+                  options: ["Black", "White", "Red"],
+                  selectedValue: selectedVehicleColor,
+                  onChanged: (value) {
+                    setState(() {
+                      selectedVehicleColor = value;
+                    });
+                  },
+                ),
+                inputFile(
+                    label: "License Plate Number",
+                    controller: licensePlateNumberController),
+                inputFile(
+                  label: "Working Location",
+                  options: [
+                    "Mbezi",
+                    "Kimara",
+                    "Ubungo",
+                    "Makongo",
+                    "Mwenge",
+                    "Kariakoo",
+                    "Makumbusho",
+                  ],
+                  selectedValue: selectedWorkingLocation,
+                  onChanged: (value) {
+                    setState(() {
+                      selectedWorkingLocation = value;
+                    });
+                  },
+                ),
+              ],
+              inputFile(
+                  label: "Password",
+                  controller: passwordController,
+                  obscureText: true),
+              inputFile(
+                  label: "Confirm Password",
+                  controller: confirmPasswordController,
+                  obscureText: true),
+              Container(
+                padding: const EdgeInsets.only(top: 3, left: 3),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(50),
+                ),
+                child: MaterialButton(
+                  minWidth: double.infinity,
+                  height: 60,
+                  onPressed: () {
+                    // Handle it locally before implement it actually
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          title: const Text("Success"),
+                          content: const Text("Account created locally!"),
+                          actions: <Widget>[
+                            TextButton(
+                              child: const Text("OK"),
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                    resetInputFields(); // Reset input fields after "registration"
+                  },
+                  color: Colors.black,
+                  elevation: 0,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                  child: const Text(
+                    "Register",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 18,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 25),
+              GestureDetector(
+                onTap: () {
+                  Navigator.pop(context);
+                },
+                child: const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text("Already have an account?"),
+                    Text(
+                      " Sign in",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blue,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 25),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+Widget inputFile({
+  required String label,
+  List<String>? options,
+  String? selectedValue,
+  void Function(String?)? onChanged,
+  bool obscureText = false,
+  TextEditingController? controller,
+}) {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: <Widget>[
+      Text(
+        label,
+        style: const TextStyle(
+          fontSize: 16,
+          fontWeight: FontWeight.bold,
+          color: Colors.black,
+        ),
+      ),
+      const SizedBox(height: 5),
+      if (options != null)
+        DropdownButtonFormField<String>(
+          dropdownColor: Colors.white,
+          value: selectedValue,
+          items: options.map((String value) {
+            return DropdownMenuItem<String>(
+              value: value,
+              child: Text(value),
+            );
+          }).toList(),
+          onChanged: onChanged,
+          decoration: const InputDecoration(
+            contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.grey),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.grey),
+            ),
+            border: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.grey),
+            ),
+          ),
+        ),
+      if (options == null)
+        TextField(
+          controller: controller,
+          obscureText: obscureText,
+          decoration: const InputDecoration(
+            contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.grey),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.grey),
+            ),
+            border: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.grey),
+            ),
+          ),
+        ),
+      const SizedBox(height: 30),
+    ],
+  );
+}
